92544c5c08b11777d45a94a3f42c6b9f1099db92
90designer <1769164516@qq.com>
2020-04-02 15:45:16 +0800

    直接使用elementui 基础知识不够扎实

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/public/' : './',
   /* 输出文件目录：在npm run build时，生成文件的目录名称 */
   outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
-  // eslint-loader 是否在保存的时候检查
-  lintOnSave: true,  
   /* 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录 */
   assetsDir: "assets",
   /* 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度 */
